81ef08edf3f59b57e6725549e318a6b0f44c3af9
raul <74162376+raulanto@users.noreply.github.com>
2023-09-14 14:52:36 -0600

    controladores faltantes

---
+++ controllers/AttandanceController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class AttandanceController extends ActiveController
+{
+    public $modelClass = 'app\models\Attandance';
+}
+
+
---
+++ controllers/TeacherController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class TeacherController extends ActiveController
+{
+    public $modelClass = 'app\models\Teacher';
+}
+
+
