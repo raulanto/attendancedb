e9cba2424483ad71cfb2256976541b4ab0f4ad32
LoyaliZA <contacto.loyal1a@gmail.com>
2023-10-03 19:50:45 -0600

    Revert "Merge branch 'Developer-1' of https://github.com/raulanto/attendancedb into Developer-1"
    This reverts commit 0b4ab8b362640511a7bd2384af0acaf523b1eb8b, reversing
    changes made to d54aa80834c8e2029b954a8b4a86af4afec88321.

---
+++ controllers/ExtraPersonController.php
@@ -0,0 +1,10 @@
+<?php
+//CONTROLADOR DE TABLA EXTRACURRICULAR_PERSON
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class ExtraPersonController extends ActiveController
+{
+    public $modelClass = 'app\models\ExtraPerson';
+}
