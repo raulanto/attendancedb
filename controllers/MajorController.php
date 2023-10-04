076b244ac856634b485fd279565033e75b26f351
LoyaliZA <contacto.loyal1a@gmail.com>
2023-10-03 19:53:25 -0600

    Adición Behaviors Gabriel Zárate
    Adición de la función behaviors a los controladores: Subject, Major y SybjectMajor
    
    Hechas desde la PC de Zárate.

--- controllers/MajorController.php
+++ controllers/MajorController.php
@@ -7,4 +7,19 @@ use yii\rest\ActiveController;
 class MajorController extends ActiveController
 {
     public $modelClass = 'app\models\Major';
+
+    public function behaviors() {
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin'                           => ['http://localhost:8100'],
+                'Access-Control-Request-Method'    => ['GET'],
+                'Access-Control-Request-Headers'   => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 600
+            ]
+        ];
+        return $behaviors;
+    }
 }
