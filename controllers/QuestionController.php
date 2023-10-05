8d18859a1e542000ce6cc501dcda93c3f32581b6
raul <74162376+raulanto@users.noreply.github.com>
2023-10-04 22:47:04 -0600

    controladores

--- controllers/QuestionController.php
+++ controllers/QuestionController.php
@@ -7,4 +7,18 @@ use yii\rest\ActiveController;
 class QuestionController extends ActiveController
 {
     public $modelClass = 'app\models\Question';
+    public function behaviors(){
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin'                           => ['http://localhost:8100'],
+                'Access-Control-Request-Method'    => ['GET','PUT','POST'],
+                'Access-Control-Request-Headers'   => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 600
+            ]
+        ];
+            return $behaviors;
+        }
 }
